fcb14320c3a50066ef26136348caaf647e3172e9
Maxim Feduishkin <maksihill@yandex.ru>
2025-01-10 20:51:40 +0500

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:spies/app.dart';
 import 'package:spies/locator.dart' as di;
@@ -7,7 +6,6 @@ import 'package:spies/locator.dart' as di;
 Future<void> main() async {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
-  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   await di.initLocator();
   FlutterNativeSplash.remove();
   runApp(App());
